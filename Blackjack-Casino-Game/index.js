c570c81cd5fd1d5effaeb783dc808c45b6925de9
Dana Alhaji <dana.alhaji95@gmail.com>
2025-05-16 16:10:50 +0300

    finish black jack game using arrays and objects

--- Blackjack-Casino-Game/index.js
+++ Blackjack-Casino-Game/index.js
@@ -1,35 +1,58 @@
 
 function getRandomNuber(){
-    return Math.floor(Math.random()*(11-2)+2)+1
+    let randomNum =  Math.floor(Math.random()*13)+1
+    if ( randomNum > 10){
+        return 10
+    }
+    else if(randomNum ===1){
+        return 11
+    }
+    else{
+        return randomNum
+    }
 }
 
+let cards = []
 
-let firstCard  = getRandomNuber()
-let secconCard = getRandomNuber()
-let cards = [firstCard, secconCard]
-
-
-let sum = firstCard+secconCard
+let sum = 0
 
 let hasBlackjack = false
-let isAlive = true
-
+let isAlive = false
 
 let messageEl =  document.getElementById("messgae-el")
 let message = " "
 let sumEl =  document.getElementById("sum-el")
 let cardsEl = document.querySelector("#card-el")
 
+
+let player = {
+    name : "per",
+    chips : 200
+
+}
+
+
+playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
 function startGame(){
+    isAlive = true
+    cards = []
+    cards.push(getRandomNuber(),getRandomNuber())
+    sum = 0
+    sum = cards[0]+cards[1]
     renderGame()
 }
 
 
 function renderGame(){ 
     // render out first & second card
-    cardsEl.textContent = "Cards: " + cards[0] + ","+ cards[1]
+    cardsEl.textContent = "Cards: " 
+    sumEl.textContent = "Sum: " + sum
+    for (let i = 0 ; i < cards.length ; i++){
+        cardsEl.textContent += cards[i] +" "
+    }
     if (sum <= 20){
-        sumEl.textContent = "Sum: " + sum
         message = "Do you want to draw a new card?"
         console.log("Do you want to draw a new card?")
 
@@ -46,11 +69,16 @@ function renderGame(){
 }
 
 function newCard(){
+    if(isAlive === true && hasBlackjack === false){
     console.log("New cards")
     let card = getRandomNuber()
-    cards.push(card)
     sum += card
+    cards.push(card)
     renderGame()
+    }
+    else{
+        messageEl.textContent = "Your out of the game , No new cards!"
+    }
 }
 console.log(firstCard , secconCard)
 console.log(hasBlackjack)
